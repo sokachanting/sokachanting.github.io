4c328b623cc0a45c8576fec7902f23585478cb85
sokachanting <46316924+sokachanting@users.noreply.github.com>
2019-01-07 18:52:41 +0800

    Update howler-rabbit.js

--- js/howler-rabbit.js
+++ js/howler-rabbit.js
@@ -1,4 +1,28 @@
-  function parseLyrics(_element) {
+function   decodeTimeStamp(timestamp) {
+    if (!timestamp || typeof timestamp !== "string") return 0;
+
+    let results;
+
+    // [hh:mm:ss.xx] format, used by some long audio books
+    results = timestamp.match(/\[(\d+):(\d+):(\d+\.\d+)\]/);
+    if (results && results.length === 4) {
+      return (
+        parseInt(results[1]) * 60 * 60 +
+        parseInt(results[2]) * 60 +
+        parseFloat(results[3])
+      );
+    }
+
+    // [mm:ss.xx] format, widely used for songs
+    results = timestamp.match(/\[(\d+):(\d+\.\d+)\]/);
+    if (results && results.length === 3) {
+      return parseInt(results[1]) * 60 + parseFloat(results[2]);
+    }
+
+    return 0;
+  }  
+
+function parseLyrics(_element) {
     
     console.log("Test");
     this.element = _element; 
